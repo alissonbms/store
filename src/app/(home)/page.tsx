f53e3369b149f625cecd2d4894186d81c13ac29c
alissonbms <alissonnewdev@gmail.com>
2024-08-06 12:56:17 -0300

    feat: change banners

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -15,15 +15,21 @@ export default async function Home() {
     <div>
       <PromotionalBanner
         imagePath="/banner-home-01.png"
-        imageDescription="55% de desconto só nesse mês!"
+        imageDescription="55% de desconto só esse mês!"
       />
 
       <div className="mt-8 px-5">
         <Categories />
       </div>
       <div className="mt-8">
+        <p className="mb-4 pl-5 font-bold uppercase">ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <PromotionalBanner
+        imagePath="/banner-home-02.png"
+        imageDescription="55% de desconto em mouses só esse mês!"
+      />
     </div>
   );
 }
