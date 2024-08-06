33133e932c3625265cffb449b17bca448436a34f
alissonbms <alissonnewdev@gmail.com>
2024-08-06 15:15:01 -0300

    chore: remove deprecated "domains" and add "remotePatterns" to next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ["utfs.io"] },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "utfs.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
